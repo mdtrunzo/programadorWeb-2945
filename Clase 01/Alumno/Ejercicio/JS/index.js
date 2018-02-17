8e61c99d986d46421c3555fb2540169d6199760e
Matias <matiasdanieltrunzo@gmail.com>
2018-02-17 12:38:16 -0300

    <Desafio>

---
+++ Clase 01/Alumno/Ejercicio/JS/index.js
@@ -0,0 +1,37 @@
+// 1 
+
+var name = prompt("Ingrese su nombre")
+
+console.log("Hola " + name + "!")
+
+// 2
+
+var age = prompt("Ingrese su edad")
+
+var number = 4
+
+var parsedAge = parseInt (age, 10)
+
+console.log(parsedAge + number)
+
+//3
+
+var age2 = prompt ("Ingrese su peso")
+
+var number2 = 10
+
+var parsedAge2 = parseInt (age2, 10)
+
+console.log(parsedAge2 - number2)
+
+//4
+
+var numero1 = prompt("Ingrese un número")
+
+var numero2 = prompt("Ingrese otro número")
+
+var parsedNumero1 = parseInt (numero1, 10)
+
+var parsedNumero2 = parseInt (numero2, 10)
+
+console.log (parsedNumero1 + parsedNumero2)
