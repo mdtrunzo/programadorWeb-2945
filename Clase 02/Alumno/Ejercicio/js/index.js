4ca43b3c4d71da6178a5c7e7c3c62b012152cf81
Matias <matiasdanieltrunzo@gmail.com>
2018-02-17 15:52:45 -0300

    <Comentario>

---
+++ Clase 02/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,20 @@
+var dayOfTheWeek = prompt("Ingresá el día de la semana")
+
+switch (dayOfTheWeek){
+        case  "Lunes":
+        case  "Martes":
+        case  "Miércoles":
+        case  "Miercoles":
+        case  "Jueves":
+        case  "Viernes":
+        console.log("Es un día hábil")
+        break
+        case "Sábado":
+        case "Sabado":
+        case "Domingo":
+        console.log("Es fin de semana")
+        break
+        default:
+        console.log("El día ingresado no es válido")
+        break
+                    }
