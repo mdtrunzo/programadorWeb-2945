6c6854e1751dd9f2d7b2ca5ad087f1ff2f045de0
Matias <matiasdanieltrunzo@gmail.com>
2018-02-19 18:39:50 -0300

    <Comentario>

---
+++ Clase 01/Alumno/Desafío/JS/index.js
@@ -0,0 +1,39 @@
+// 1
+
+var name = prompt("Elija un número y será sumado a 3")
+
+var number = 3
+
+var parsedNumber = parseInt(name,10)
+
+console.log("La suma es=", parsedNumber + number)
+
+// 2
+
+var name = prompt("Elija otro número y será restado a 3")
+
+var number = 3
+
+var parsedNumber = parseInt(name,10)
+
+console.log("La resta es=", parsedNumber - number)
+
+// 3
+
+var name = prompt("Elija otro número y será multiplicado por 3")
+
+var number = 3
+
+var parsedNumber = parseInt (name,10)
+
+console.log("La multiplicación es=", parsedNumber * number)
+
+// 4
+
+var name = prompt("Elija otro número y será dividido por 3")
+
+var number = 3
+
+var parsedNumber = parseInt (name,10)
+
+console.log("la división es=", parsedNumber / number)
---
+++ Clase 02/Alumno/Desafío/js/index.js
@@ -0,0 +1,7 @@
+var daysOfTheWeek = ["Lunes", "Martes", "Miercoles","Jueves", "Viernes", "Sabado", "Domingo"]
+
+
+for(var i = 0; i < daysOfTheWeek.length; i++){
+    var day = daysOfTheWeek[i]
+    console.log(day)
+    
