554c8147bc0df654d12e6cef8ff9b8b698f77c49
Matias <matiasdanieltrunzo@gmail.com>
2018-02-26 22:04:27 -0300

    Desafio

---
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -0,0 +1,48 @@
+//1
+
+var number1 = 5
+var number2 = 4
+
+var parsedNumber = parseInt(number1,10)
+var parsedNumber2 = parseInt(number2,10)
+
+function sum(parsedNumber, parsedNumber2) {
+    var sum = parsedNumber + parsedNumber2
+    return sum
+  }
+  var result = parsedNumber + parsedNumber2
+
+  console.log(result)
+
+ //
+
+ function resta(parsedNumber, parsedNumber2) {
+    var resta= parsedNumber - parsedNumber2
+    return resta
+  }
+  var result = parsedNumber - parsedNumber2
+
+  console.log(result)
+
+  //
+
+ function mult(parsedNumber, parsedNumber2) {
+    var mult= parsedNumber * parsedNumber2
+    return mult
+  }
+  var result = parsedNumber * parsedNumber2
+
+  console.log(result)
+
+  //
+
+ function div(parsedNumber, parsedNumber2) {
+    var div= parsedNumber / parsedNumber2
+    return div
+  }
+  var result = parsedNumber / parsedNumber2
+
+  console.log(result)
+
+  2//
+  
