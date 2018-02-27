9eca6d319154182a72c18073e7972101684c58c6
Matias <matiasdanieltrunzo@gmail.com>
2018-02-26 23:09:57 -0300

    Desafio

--- Clase 03/Alumno/Desafío/js/index.js
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -1,4 +1,4 @@
-//1
+// # Convertir las operaciones de la calculadora en pequeñas funciones
 
 var number1 = 5
 var number2 = 4
@@ -44,5 +44,48 @@ function sum(parsedNumber, parsedNumber2) {
 
   console.log(result)
 
-  2//
+  // # Crear una función que devuelva la operación pedida
   
+  var Operation = prompt("Ingrese suma, resta, división o multiplicación")
+
+  var number1 = prompt("Elija un número")
+
+  var number2 = prompt("Elija otro número")
+
+  var parsedNumber = parseInt(number1,10)
+
+  var parsedNumber2 = parseInt(number2,10)
+
+  function askForOperation(Operation){
+    switch (Operation) {
+      case "suma": 
+      case "Suma":
+      console.log("La suma es: ", parsedNumber + parsedNumber2)
+      break
+      case "resta": 
+      case "Resta":
+      console.log("La resta es: ", parsedNumber - parsedNumber2)
+      break
+      case "División": 
+      case "división":
+      case "Division":
+      case "division":
+      console.log("la división es: ", parsedNumber / parsedNumber2)
+      if (parsedNumber === 0 || parsedNumber2 === 0 ){
+        console.log(prompt("El número ingresado no puede ser igual a 0"))
+    }
+      break
+      case "Multiplicación": 
+      case "multiplicación":
+      case "Multiplicacion":
+      case "multiplicacion":
+      console.log("La multiplicación es: ", parsedNumber * parsedNumber2)
+      break
+    }
+  }
+  askForOperation(Operation)
+
+  // (Usar las funciones del punto anterior para calcular el 
+  //resultado según corresponda y mostrar en pantalla el mismo.)
+
+
