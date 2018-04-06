c2c0e792be53030c03bcea2e3ec03d09fb2d1505
Matias <matiasdanieltrunzo@gmail.com>
2018-04-06 01:12:15 -0300

    Desafio

--- Clase 09/Alumno/Desafío/js/index.js
+++ Clase 09/Alumno/Desafío/js/index.js
@@ -13,7 +13,7 @@ textInputName.onblur = function (event) {
     parentPasswordInputNode.removeChild(passwordErrorNode)
   }
 
-if (value) {
+  if (value) {
     textInputNameNode.classList.remove('is-invalid')
     textInputNameNode.classList.add('is-valid')
   } else {
@@ -27,9 +27,8 @@ if (value) {
 
     parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
-
 
 var textInputLastName = document.getElementById('textInputLastName')
 
@@ -38,14 +37,25 @@ textInputLastName.onblur = function (event) {
 
   var value = textInputLastNameNode.value
 
-if (value) {
+  var parentPasswordInputNode = textInputLastName.parentElement
+
+  var passwordErrorNode = document.getElementById('passwordError')
+
+  if (value) {
     textInputLastNameNode.classList.remove('is-invalid')
     textInputLastNameNode.classList.add('is-valid')
   } else {
     textInputLastNameNode.classList.remove('is-valid')
     textInputLastNameNode.classList.add('is-invalid')
+
+    passwordErrorNode = document.createElement('span')
+
+    passwordErrorNode.id = 'passwordError'
+    passwordErrorNode.innerHTML = 'El campo no puede estar vacío'
+
+    parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
 
 var textInputMail = document.getElementById('textInputMail')
@@ -55,18 +65,29 @@ textInputMail.onblur = function (event) {
 
   var value = textInputMailNode.value
 
+  var parentPasswordInputNode = textInputMail.parentElement
 
-if (value && typeof value === 'string' && value.indexOf('@') !== -1 && value.indexOf('.') !== -1) {
+  var passwordErrorNode = document.getElementById('passwordError')
+
+  if (
+    value &&
+    typeof value === 'string' &&
+    value.indexOf('@') !== -1 &&
+    value.indexOf('.') !== -1
+  ) {
     textInputMailNode.classList.remove('is-invalid')
     textInputMailNode.classList.add('is-valid')
   } else {
     textInputMailNode.classList.remove('is-valid')
     textInputMailNode.classList.add('is-invalid')
+
+    passwordErrorNode.id = 'passwordError'
+    passwordErrorNode.innerHTML = 'Tiene que contener "@"'
+
+    parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
-
-
 
 var password1 = document.getElementById('password1')
 
@@ -75,14 +96,23 @@ password1.onblur = function (event) {
 
   var value = password1Node.value
 
-if (value && typeof value === 'string' && value.length >= 6) {
+  var parentPasswordInputNode = password1.parentElement
+
+  var passwordErrorNode = document.getElementById('passwordError')
+
+  if (value && typeof value === 'string' && value.length >= 6) {
     password1Node.classList.remove('is-invalid')
     password1Node.classList.add('is-valid')
   } else {
     password1Node.classList.remove('is-valid')
     password1Node.classList.add('is-invalid')
+
+    passwordErrorNode.id = 'passwordError'
+    passwordErrorNode.innerHTML = 'Debe contener por lo menos 6 caracteres'
+
+    parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
 
 var password2 = document.getElementById('password2')
@@ -91,15 +121,23 @@ password2.onblur = function (event) {
   var password2Node = event.target
 
   var value = password2Node.value
+  var parentPasswordInputNode = password2.parentElement
 
-if (value && typeof value === 'string' && value.length >= 6) {
+  var passwordErrorNode = document.getElementById('passwordError')
+
+  if (value && typeof value === 'string' && value.length >= 6) {
     password2Node.classList.remove('is-invalid')
     password2Node.classList.add('is-valid')
   } else {
     password2Node.classList.remove('is-valid')
     password2Node.classList.add('is-invalid')
+
+    passwordErrorNode.id = 'passwordError'
+    passwordErrorNode.innerHTML = 'Debe contener por lo menos 6 caracteres'
+
+    parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
 
 var legalAge = document.getElementById('legalAge')
@@ -109,26 +147,33 @@ legalAge.onblur = function (event) {
 
   var value = legalAgeNode.value
 
-if (value && typeof value === "string" && value.length >= 18) {
+  var parentPasswordInputNode = legalAge.parentElement
+
+  var passwordErrorNode = document.getElementById('passwordError')
+
+  if (value && typeof value === 'string' && value >= 18) {
     legalAgeNode.classList.remove('is-invalid')
     legalAgeNode.classList.add('is-valid')
   } else {
     legalAgeNode.classList.remove('is-valid')
     legalAgeNode.classList.add('is-invalid')
+
+    passwordErrorNode.id = 'passwordError'
+    passwordErrorNode.innerHTML = 'Debe ser mayor de 18 años'
+
+    parentPasswordInputNode.appendChild(passwordErrorNode)
   }
-  validateAllFields ()
+  validateAllFields()
 }
 
-
-
 function validateAllFields () {
-    var submitButtom = document.getElementById('submitButtom')
-  
-    var elementsWithIsInvalid = document.getElementsByClassName('is-valid')
-  
-    if (elementsWithIsInvalid && elementsWithIsInvalid.length === 6) {
-      submitButtom.disabled = false
-    } else {
-      submitButtom.disabled = true
-    }
+  var submitButtom = document.getElementById('submitButtom')
+
+  var elementsWithIsInvalid = document.getElementsByClassName('is-valid')
+
+  if (elementsWithIsInvalid && elementsWithIsInvalid.length === 6) {
+    submitButtom.disabled = false
+  } else {
+    submitButtom.disabled = true
   }
+}
