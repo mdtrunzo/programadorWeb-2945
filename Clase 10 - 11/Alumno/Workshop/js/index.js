d040d7e4aac0b32d84e5d619eb4a718257deedb7
Matias <matiasdanieltrunzo@gmail.com>
2018-04-10 21:34:08 -0300

    Workshop

--- Clase 10 - 11/Alumno/Workshop/js/index.js
+++ Clase 10 - 11/Alumno/Workshop/js/index.js
@@ -1,121 +1,163 @@
-var firstName = document.getElementById('firstName')
+var addStudentButton = document.getElementById('addStudentButton')
 
-firstName.onblur = function (event) {
+addStudentButton.disabled = true
+
+var deleteStudentButton = document.getElementById('deleteStudentButton')
+
+deleteStudentButton.disabled = true
+
+function init () {
+  var mainList = document.getElementById('mainList')
+  var studentsList = getLocalStorageList()
+
+  for (var i = 0; i < studentsList.length; i++) {
+    var studentNode = createStudentNodeSimple(studentsList[i])
+    mainList.appendChild(studentNode)
+  }
+}
+
+init()
+
+function createStudentNodeSimple (student) {
+  var li = document.createElement('li')
+
+  li.innerHTML =
+    '<h1>' + student.firstName + '<h1><h3>DNI: ' + student.dni + '<h3>'
+
+  li.className = 'list-group-item'
+
+  li.id = student.dni
+
+  return li
+}
+
+function validateButtonSimple () {
+  var validFields = document.getElementsByClassName('is-valid')
+  if (validFields.length === 2) {
+    addStudentButton.disabled = false
+  } else {
+    addStudentButton.disabled = true
+  }
+}
+
+document.getElementById('firstName').onblur = function (event) {
   var firstNameNode = event.target
 
-  var value = firstNameNode.value
-
-  if (value) {
+  if (firstNameNode.value) {
     firstNameNode.classList.remove('is-invalid')
     firstNameNode.classList.add('is-valid')
   } else {
     firstNameNode.classList.remove('is-valid')
     firstNameNode.classList.add('is-invalid')
   }
-  validateAllFields()
+
+  validateButtonSimple()
 }
 
-var dni = document.getElementById('dni')
-
-dni.onblur = function (event) {
+document.getElementById('dni').onblur = function (event) {
   var dniNode = event.target
 
   var value = dniNode.value
 
-  if (value && typeof value === 'string' && value >= 1) {
+  var parsedDni = parseInt(value, 10)
+
+  if (
+    parsedDni &&
+    !isNaN(parsedDni) &&
+    parsedDni >= 0 &&
+    searchIndexByDni(parsedDni) === -1
+  ) {
     dniNode.classList.remove('is-invalid')
     dniNode.classList.add('is-valid')
   } else {
     dniNode.classList.remove('is-valid')
     dniNode.classList.add('is-invalid')
   }
-  validateAllFields()
+
+  validateButtonSimple()
 }
 
-var dni = document.getElementById('deleteDni')
+function addStudentSimple (firstName, dni) {
+  var mainList = document.getElementById('mainList')
 
-dni.onblur = function (event) {
+  var newStudent = {
+    firstName: firstName,
+    dni: dni
+  }
+
+  var studentsList = getLocalStorageList()
+
+  studentsList.push(newStudent)
+
+  setLocalStorageList(studentsList)
+
+  var studentNode = createStudentNodeSimple(newStudent)
+  mainList.appendChild(studentNode)
+}
+
+addStudentButton.onclick = function (event) {
+  var firstNameNode = document.getElementById('firstName')
+  var dniNode = document.getElementById('dni')
+  var parsedDni = parseInt(dniNode.value, 10)
+
+  addStudentSimple(firstNameNode.value, parsedDni)
+
+  firstNameNode.classList.remove('is-valid')
+  dniNode.classList.remove('is-valid')
+
+  addStudentButton.disabled = true
+
+  firstNameNode.value = ''
+  dniNode.value = ''
+}
+
+document.getElementById('deleteDni').onblur = function (event) {
   var dniNode = event.target
 
   var value = dniNode.value
 
-  if (value && typeof value === 'string' && value >= 1) {
-    dniNode.classList.remove('is-invalid')
-    dniNode.classList.add('is-valid')
+  var parsedDni = parseInt(value, 10)
+
+  if (searchIndexByDni(parsedDni) !== -1) {
+    deleteStudentButton.disabled = false
   } else {
-    dniNode.classList.remove('is-valid')
-    dniNode.classList.add('is-invalid')
-  }
-  validateAllFields()
-}
-
-function validateAllFields () {
-  var submitButtom = document.getElementById('addStudentButton')
-
-  var elementsWithIsInvalid = document.getElementsByClassName('is-valid')
-
-  if (elementsWithIsInvalid && elementsWithIsInvalid.length === 2) {
-    submitButtom.disabled = false
-  } else {
-    submitButtom.disabled = true
+    deleteStudentButton.disabled = true
   }
 }
 
-/*
-var students = [
-  {
-    firstName: 'Matias',
-    dni: 34813447
-  },
-  {
-    firstName: 'Javier',
-    dni: 12345678
-  }
-]
+deleteStudentButton.onclick = function () {
+  var dniNode = document.getElementById('deleteDni')
 
-var stringList = JSON.stringify(students)
+  var value = dniNode.value
 
-localStorage.setItem('students', stringList)
+  var studenNode = document.getElementById(value)
 
-function showStudents (studentsList) {
-  var student
-  var rootContainer = document.getElementById('mainList')
-  rootContainer.innerHTML = '<ul class="list-group" id="list"></ul>'
-  var listContainer = document.getElementById('list')
-  for (var i = 0; i < studentsList.length; i++) {
-    student = students[i]
+  var mainList = document.getElementById('mainList')
 
-    var liStudent = createStudentLi(student.firstName, student.dni)
+  mainList.removeChild(studenNode)
 
-    listContainer.appendChild(liStudent)
-  }
+  var parsedDni = parseInt(value, 10)
+
+  var studentsList = getLocalStorageList()
+
+  var index = searchIndexByDni(parsedDni)
+
+  studentsList.splice(index, 1)
+
+  setLocalStorageList(studentsList)
+
+  deleteStudentButton.disabled = true
+
+  dniNode.value = ''
 }
 
-function createStudentLi (firstName, dni) {
-  var li = document.createElement('li')
-
-  li.className = 'list-group-item '
-
-  li.id = dni
-
-  li.innerHTML = '<h1>' + firstName + '</h1><p>' + dni + '</p>'
-
-  return li
-}
-
-showStudents(students)
-
-function searchIndexByText (text, studentsList) {
+function searchIndexByDni (dni) {
   var index = -1
+  var studentsList = getLocalStorageList()
 
   for (var i = 0; i < studentsList.length; i++) {
     var student = studentsList[i]
-    if (
-      text &&
-      typeof text === 'string' &&
-      (student.firstName.toUpperCase().indexOf(text.toUpperCase()) !== -1 ||
-        student.lastName.toUpperCase().indexOf(text.toUpperCase()) !== -1)
-    ) {
+    if (student.dni === dni) {
       index = i
       break
     }
@@ -123,15 +165,17 @@ function searchIndexByText (text, studentsList) {
 
   return index
 }
-*/
 
-function getLocalStorage (text) {
-  var getFirstName = document.getElementById('firstName')
-  var getDni = document.getElementById('dni')
+function getLocalStorageList () {
+  var studentsList = localStorage.getItem('studentsList')
 
-  if (text && typeof text === 'string') {
-    var stringList = JSON.stringify(students)
+  var parsedList = studentsList ? JSON.parse(studentsList) : []
 
-    localStorage.setItem('students', stringList)
-  }
+  return parsedList
+}
+
+function setLocalStorageList (studentsList) {
+  var stringList = JSON.stringify(studentsList)
+
+  localStorage.setItem('studentsList', stringList)
 }
