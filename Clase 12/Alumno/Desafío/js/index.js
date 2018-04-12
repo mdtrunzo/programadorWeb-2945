2f9fb35babd9ce9fb5a8cf40b66068af3156e7a6
Matias <matiasdanieltrunzo@gmail.com>
2018-04-12 19:53:23 -0300

    desafio

---
+++ Clase 12/Alumno/Desafío/js/index.js
@@ -0,0 +1,66 @@
+var inputTextNodes = $('input[type="text"]')
+
+inputTextNodes.blur(function (event) {
+  var inputTextNode = $(this)
+
+  var value = inputTextNode.val()
+
+  if (value) {
+    inputTextNode.removeClass('is-invalid')
+    inputTextNode.addClass('is-valid')
+  } else {
+    inputTextNode.removeClass('is-valid')
+    inputTextNode.addClass('is-invalid')
+  }
+  validateButtonSimple()
+})
+
+var inputMailNodes = $('input[type="email"]')
+
+inputMailNodes.blur(function (event) {
+  var inputMailNode = $(this)
+
+  var value = inputMailNode.val()
+
+  if (
+    value &&
+    typeof value === 'string' &&
+    value.indexOf('@') !== -1 &&
+    value.indexOf('.') !== -1
+  ) {
+    inputMailNode.removeClass('is-invalid')
+    inputMailNode.addClass('is-valid')
+  } else {
+    inputMailNode.removeClass('is-valid')
+    inputMailNode.addClass('is-invalid')
+  }
+  validateButtonSimple()
+})
+
+var inputCommentsNodes = $('#comments')
+
+inputCommentsNodes.blur(function (event) {
+  var inputCommentsNode = $(this)
+
+  var value = inputCommentsNode.val()
+
+  if (value) {
+    inputCommentsNode.removeClass('is-invalid')
+    inputCommentsNode.addClass('is-valid')
+  } else {
+    inputCommentsNode.removeClass('is-valid')
+    inputCommentsNode.addClass('is-invalid')
+  }
+  validateButtonSimple()
+})
+
+$('#submitButton').attr('disabled', true)
+
+function validateButtonSimple () {
+  var validFields = $('.is-valid')
+  if (validFields.length === 3) {
+    $('#submitButton').attr('disabled', false)
+  } else {
+    $('#submitButton').attr('disabled', true)
+  }
+}
